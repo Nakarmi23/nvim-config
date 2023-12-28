2cf5deb208c6ec5452cce8bbeb2e88910add60a2
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2023-12-28 17:52:21 +0545

    feat: change tab width to 2

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,48 +1,48 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 
 
 require('mason').setup({
-    ensure_installed = { 'prettier' }
+  ensure_installed = { 'prettier' }
 })
 require('mason-lspconfig').setup({
-    -- Replace the language servers listed here
-    -- with the ones you want to install
-    ensure_installed = { 'tsserver', 'eslint', 'lua_ls' },
-    handlers = {
-        lsp_zero.default_setup,
-    },
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = { 'tsserver', 'eslint', 'lua_ls' },
+  handlers = {
+    lsp_zero.default_setup,
+  },
 })
 
 local cmp = require('cmp')
 cmp.setup({
 
-    mapping = cmp.mapping.preset.insert({
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    })
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  })
 })
 
 lsp_zero.on_attach(function(client, bufnr)
-    local otps = { buffer = bufnr, remap = false }
+  local otps = { buffer = bufnr, remap = false }
 
-    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, otps)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, otps)
-    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-    -- vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', '<leader>vr', function() require('telescope.builtin').lsp_references() end, opts)
-    vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, otps)
+  vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, otps)
+  vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+  -- vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set('n', '<leader>vr', function() require('telescope.builtin').lsp_references() end, opts)
+  vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 
-    lsp_zero.buffer_autoformat()
+  lsp_zero.buffer_autoformat()
 end)
 
 lsp_zero.setup()
--- lua/nakarmi23/set.lua
+++ lua/nakarmi23/set.lua
@@ -1,9 +1,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop =4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -24,4 +24,4 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
