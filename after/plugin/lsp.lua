3b07cc7576b48aae45b0f8c92e346c643d2a6ef5
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2024-04-22 14:17:47 +0545

    feat: add lsp support for tailwindcss, css, gtml,sql, yaml and markdown

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,7 +14,7 @@ require('mason').setup({
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = { 'tsserver', 'eslint', 'lua_ls' },
+  ensure_installed = { 'tsserver', 'eslint', 'lua_ls', 'tailwindcss', 'cssls', 'html', 'mdx_analyzer', 'sqlls', 'yamlls' },
   handlers = {
     lsp_zero.default_setup,
   },
